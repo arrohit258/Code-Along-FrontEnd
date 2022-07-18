b5a4f8bc44c65dfd38403c70f5d4622844cb1630
Rohit Arora <arrohit258@gmail.com>
2022-07-18 11:20:00 +0530

    make it better

---
+++ router.js
@@ -0,0 +1,36 @@
+
+const express=require('express')
+const router=express.Router();
+const axios = require('axios');
+
+
+
+router.post("/getOutput",(req,res)=>{
+    const {code,language,input}=req.body
+    //console.log(req.body)
+    const data ={
+           code,
+           language,
+           input
+    }
+    console.log(data);
+
+
+axios.post('https://codex-api.herokuapp.com/',data)
+.then(function (response) {
+    
+  const output=response.data.output
+
+  return res.send(output)
+})
+.catch(function (error) {
+  console.log("error");
+});
+    
+   
+})
+
+module.exports=router
+
+//axios.post('https://codexweb.netlify.app/.netlify/functions/enforceCode',data)
+//
